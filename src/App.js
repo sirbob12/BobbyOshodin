a22cb6028340310651db73676e565aa7fab6997b
bobby <sirbob12@gmail.com>
2022-07-10 09:02:10 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import {Routes, Route} from 'react-router-dom';
+import {Home} from './Pages';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>new app</h1>
+      <Home/>
     </div>
   );
 }
