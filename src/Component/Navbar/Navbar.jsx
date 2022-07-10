194331ae4f3e71ff5bceb80368a24701938013ee
bobby <sirbob12@gmail.com>
2022-07-10 15:52:00 -0700

    commit

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {images} from '../../Constant'
+import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+     <nav className="navbar">
+      <div className="container flex">
+       <a href="/Home"> <img src={images.logo} alt="logo" className="logo_image" /></a>
+
+        <ul className="navbar_elements_wrapper">
+          <a href="#"><li classname='list_elements'>About</li></a>
+          <a href="#"><li classname='list_elements'>Skills</li></a>
+          <a href="#"><li classname='list_elements'>Portfolio</li></a>
+        </ul>
+      </div>
+     </nav>
   )
 }
 
-export default Navbar
+export default Navbar 
---
+++ src/Constant/images.js
@@ -0,0 +1,8 @@
+import logo from '../port_asset/logo.svg';
+
+
+//export images asset
+
+export default{
+    logo
+}
