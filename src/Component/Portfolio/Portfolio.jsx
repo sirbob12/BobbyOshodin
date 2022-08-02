48e16ef54675845e4c2018e3f8875a29735eb3fd
bobby <sirbob12@gmail.com>
2022-08-01 20:08:29 -0700

    new commit

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -8,28 +8,39 @@ const portfolio = () => {
         <div className="portfolio_wrapper">
             <h1 className="portfolio_heading">My Portfolio</h1>
                <div className="portfolio_container">
+                
                 <div className="portfolio_card1">
                     <img src={images.app1} alt="image" className="app1_img" />
 
                     <div className="text_btn_wrapper">
-                    <p className="app1_heading">Social-Media</p>
-                    <a href="#" className="btn">View Design</a>
+                    <h1 className="app1_heading">Social-Media</h1>
+                    <p className="_text_captions">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, suscipit architecto? Totam, architecto est dignissimos, unde alias voluptatem saepe assumenda impedit dolore aliquam cupiditate possimus ad excepturi sit, qui et!
+                    </p>
+                    <a href="#" className="btn">See More</a>
                     </div>
                 </div>
-                <div className="portfolio_card1">
+                <div className="portfolio_card2">
 
-                    <img src={images.app2} alt="image" className="app1_img" />
+                    
                     <div className="text_btn_wrapper">
-                    <p className="app1_heading">Social-Media</p>
-                    <a href="#" className="btn">View Design</a>
+                    <h1 className="app1_heading">Social-Media</h1>
+                    <p className="_text_captions">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, suscipit architecto? Totam, architecto est dignissimos, unde alias voluptatem saepe assumenda impedit dolore aliquam cupiditate possimus ad excepturi sit, qui et!
+                    </p>
+                    <a href="#" className="btn">See More</a>
                     </div>
+                    <img src={images.app2} alt="image" className="app2_img" />
                 </div>
 
-                <div className="portfolio_card1">
-                    <img src={images.app3} alt="image" className="app1_img" />
+                <div className="portfolio_card3">
+                    <img src={images.app3} alt="image" className="app3_img" />
                     <div className="text_btn_wrapper">
-                    <p className="app1_heading">Social-Media</p>
-                    <a href="#" className="btn">View Design</a>
+                    <h1 className="app1_heading">Social-Media</h1>
+                    <p className="_text_captions">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, suscipit architecto? Totam, architecto est dignissimos, unde alias voluptatem saepe assumenda impedit dolore aliquam cupiditate possimus ad excepturi sit, qui et!
+                    </p>
+                    <a href="#" className="btn">See More</a>
                     </div>
                 </div>
               
